6c9d554252e1b035ff3fac35b4f89cb4168529fe
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-11-09 21:48:34 -0300

    change folder three
    minor cleanup

---
+++ src/app/(client)/loggedin/page.tsx
@@ -0,0 +1,7 @@
+export default function ClientPage (){
+  return (
+    <div>
+      Client Page (logged in url)
+    </div>
+  )
+}
--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-// This is just for test. TODO: Remove this file
+//TODO This is just for test. Remove this file
 export async function GET() {
   return NextResponse.json({ message: "Hello from the API!" });
 }
