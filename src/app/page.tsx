59b7b1c6d0483ca3517a381d4d985592d8324ba6
Frankdias92 <frankmcdias@gmail.com>
2024-11-11 20:09:04 -0300

    :sparkles: feat: Adding calendar page and appointment components
    - Created `AppointmentUserForm` for user input with validation via `zod`
    - Added `Appointment` and `AppointmentHeaderStatus` components for user goal setting and progress display
    - Updated `page.tsx` for main and calendar to integrate new components
    - Enhanced `TextLabel` for consistent text styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,42 +3,10 @@
 import Home from "@/components/home/Home"
 
 export default function Dashboard() {
-
-  // async function handleRegister() {
-  //   try {
-  //     const newUser = {
-  //       isAdmin: false,
-  //       email: "userSippy3@example.com",
-  //       phoneNumber: "123456789",
-  //       firstName: "Sippy Test",
-  //       lastName: "Dev?",
-  //       // addressId: "Los Angeles"
-  //     };
-    
-  //     const response = await fetch('/api/users', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify(newUser)
-  //     });
-    
-  //     if (!response.ok) {
-  //       throw new Error(`Error: ${response.status} ${response.statusText}`);
-  //     }
-
-  //     const result = await response.json();
-  //     console.log("User created",result.data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
   
   return (
     <div className="flex justify-center w-full h-full">
       <Home />
-
-      <button >test new user</button>
     </div>
   );
 }
--- src/components/appointmentCalendar/AppointmentUserForm.tsx
+++ src/components/appointmentCalendar/AppointmentUserForm.tsx
@@ -6,55 +6,62 @@ import { ButtonText } from "../buttonText";
 import { InputText } from "../inputText";
 import { TextLabel } from "../textLabel";
 import { WrapperContainer } from "../wrapperConteiner";
+import { HeaderStatus } from "./AppointmentHeaderStatus";
 
 const formSchema = z.object({
-  userName: z.string().min(3, { message: 'Name must be at least 2 characters long' }).transform(name => name.toLowerCase()),
-  fullName: z.string().min(3, { message: 'Name must be at least 2 characters long' }).transform(name => name.toLowerCase())
-
+  userName: z.string()
+    .min(3, { message: 'Your username must be at least 3 characters long' })
+    .transform(name => name.toLowerCase()),
+  fullName: z.string()
+    .min(3, { message: 'Name must be at least 3 characters long' })
+    .transform(name => name.toLowerCase())
 });
 
 type FormSchemaUserFormData = z.infer<typeof formSchema>;
 
 export function AppointmentUserForm() {
   const [formData, setFormData] = useState<FormSchemaUserFormData>({ userName: '', fullName: '' });
-  const [error, setError] = useState<string | null>(null);
+  const [errors, setErrors] = useState<{ userName?: string; fullName?: string }>({});
 
   async function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
     const result = formSchema.safeParse(formData);
 
     if (!result.success) {
-      setError(result.error.errors[0]?.message); // Set error message
-      return;
+      const fieldErrors: { userName?: string; fullName?: string } = {};
+      result.error.errors.forEach(err => {
+        fieldErrors[err.path[0] as keyof FormSchemaUserFormData] = err.message;
+      });
+      setErrors(fieldErrors);
+    } else {
+      console.log(result.data.userName, result.data.fullName); // Valid data
+      setErrors({});
     }
-
-    console.log(result.data.userName, result.data.fullName); // Valid data here
-    setError(null); // Clear errors
   }
 
   return (
-    <div className="flex flex-1 justify-center items-center">
-      
+    <div className="flex w-[540px] gap-2 flex-col flex-1 justify-center items-center">
+      <HeaderStatus currentStage={0} stages={['1', '2', '3', '4']}/>
       <form
         onSubmit={handleSubmit}
-        className="flex p-6 w-[510px] flex-col gap-4 bg-simmpy-gray-600 rounded-md"
+        className="flex p-6 w-full flex-col gap-4 bg-simmpy-gray-600 rounded-md"
       >
         <WrapperContainer>
-          <TextLabel text="Name" fontSize="Text-18" color="Gray-100" />
+          <TextLabel text="Username" fontSize="Text-18" color="Gray-100" />
           <InputText 
             text="userName"
             value={formData.userName}
             onChange={(event) => setFormData({ ...formData, userName: event.target.value })}
-            error={error ?? ''}
+            error={errors.userName}
             preFix
           />
 
-          <TextLabel text="fullName" fontSize="Text-18" color="Gray-100" />
+          <TextLabel text="Full Name" fontSize="Text-18" color="Gray-100" />
           <InputText 
             text="fullName"
             value={formData.fullName}
             onChange={(event) => setFormData({ ...formData, fullName: event.target.value })}
-            error={error ?? ''}
+            error={errors.fullName}
           />
         </WrapperContainer> 
 
