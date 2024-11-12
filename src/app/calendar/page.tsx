7286367fde540e32ad9e1e0d6718103090a7eb2b
Frankdias92 <frankmcdias@gmail.com>
2024-11-11 21:40:38 -0300

    :wastebasket: Rever: rollback for Initial Project Structure :boom:

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,12 +1,10 @@
 
-import { AppointmentUserForm } from "@/components/appointmentCalendar/AppointmentUserForm";
-
 
 export default function Calendar() {
 
   return (
     <div className="flex w-full flex-col h-screen justify-start items-center">
-      <AppointmentUserForm />
+      {/* page calendar / Appoiment */}
     </div>
   )
 }
