68c82199ec0d74908a805e6d2d37d96c1c17bea3
Frankdias92 <frankmcdias@gmail.com>
2024-11-18 11:56:59 -0300

    :recycle: Revert: add new calendar components and update calendar page
    - Updated src/app/calendar/page.tsx for calendar functionality.
    - Added new components:
      - Appointment: Handles appointment logic.
      - AppointmentHeaderStatus: Displays the status header for appointments.
      - AppointmentUserForm: Manages user inputs for appointments.
      - buttonText.tsx: Reusable button component.
      - inputText.tsx: Reusable input field component.
      - textLabel.tsx: Label component for consistent styling.
      - wrapperContainer.tsx: Layout wrapper component for content organization.

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,10 +1,11 @@
+import { AppointmentUserForm } from "@/components/appointmentCalendar/AppointmentUserForm";
 
 
 export default function Calendar() {
 
   return (
     <div className="flex w-full flex-col h-screen justify-start items-center">
-      {/* page calendar / Appoiment */}
+      <AppointmentUserForm />
     </div>
   )
 }
---
+++ src/components/appointmentCalendar/AppointmentHeaderStatus.tsx
@@ -0,0 +1,39 @@
+
+
+import { TextLabel } from '../textLabel';
+
+interface StageProgressProps {
+  stages: string[];
+  currentStage: number; // index of the active stage
+}
+
+export function HeaderStatus({ stages, currentStage }: StageProgressProps) {
+  return (
+    <>  
+      <TextLabel 
+        text='We need some information to create your profile! Oh, and you can edit this information later.'
+        color='Gray-600'
+        fontSize='Text-20'
+      />
+      <div className='flex flex-col w-full pt-4'>
+        <TextLabel 
+          text={`${currentStage + 1} of ${stages.length}`}
+          color='Gray-600'
+          fontSize='Text-14'
+        />
+        <div className="grid grid-flow-col w-full items-center space-x-4">
+          {stages.map((stage, index) => (
+            <div key={stage} className="flex items-center">
+              <div
+                className={`h-1.5 w-full rounded-full flex items-center justify-center ${
+                  index <= currentStage ? 'bg-simmpy-gray-100' : 'bg-simmpy-gray-600'
+                }`}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
