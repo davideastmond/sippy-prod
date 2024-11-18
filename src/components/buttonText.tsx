471f70c69747c4be9facd821836bf9e996477d36
David Eastmond <davebuck1981@gmail.com>
2024-11-18 18:44:57 -0500

    lint fix, add name

--- src/components/buttonText.tsx
+++ src/components/buttonText.tsx
@@ -1,22 +1,32 @@
-
-
 interface ButtonTextProps {
-  text: string
-  color: 'Green' | 'Yellow'
-  onClick?: () => void
+  text: string;
+  color: "Green" | "Yellow";
+  name?: string;
+  onClick?: () => void;
 }
 
-export function ButtonText({ text, onClick, color, ...rest }: ButtonTextProps) {
+export function ButtonText({
+  text,
+  onClick,
+  color,
+  name,
+  ...rest
+}: ButtonTextProps) {
   return (
     <button
       className={`flex justify-center items-center font-montserrat font-bold text-xl w-full py-2 rounded-lg
-        ${color === 'Green' ? 'bg-simmpy-green text-white' : 'bg-simmpy-yellow text-simmpy-gray-800' }
+        ${
+          color === "Green"
+            ? "bg-simmpy-green text-white"
+            : "bg-simmpy-yellow text-simmpy-gray-800"
+        }
       `}
       onClick={onClick}
+      name={name}
       type="submit"
       {...rest}
     >
       {text}
     </button>
-  )
+  );
 }
