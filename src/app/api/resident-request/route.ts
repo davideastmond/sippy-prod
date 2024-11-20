5ed4291f2dec728ed7aacdcedef08c205e83aeda
David Eastmond <davebuck1981@gmail.com>
2024-11-20 18:25:43 -0500

    reading the request body

--- src/app/api/resident-request/route.ts
+++ src/app/api/resident-request/route.ts
@@ -2,9 +2,12 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export const POST = auth(async function POST(req) {
-  if (req.auth) {
-    console.log(req.auth?.user);
-    return NextResponse.json({ message: "Hello" });
+  if (!req.auth) {
+    return NextResponse.json({ message: "No session" }, { status: 401 });
   }
-  return NextResponse.json({ message: "No session" }, { status: 401 });
+
+  const requestBody = await req.json();
+
+  console.info("Request body", requestBody);
+  return NextResponse.json({ message: "Request received" });
 });
