aa41ceb51bd6d022503c451b23ffa26183d12088
David Eastmond <davebuck1981@gmail.com>
2024-11-23 07:51:28 -0500

    resident request POST route

--- src/app/api/resident-request/route.ts
+++ src/app/api/resident-request/route.ts
@@ -1,4 +1,8 @@
 import { auth } from "@/auth";
+import { prisma } from "@/lib/prisma";
+import { assignRequestedTimeSlot } from "@/lib/utils/time-slot/time-slot-assigners";
+import { ResidentReqestApiRequest } from "@/types/resident-request-api-request";
+import { Address } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export const POST = auth(async function POST(req) {
@@ -6,8 +10,66 @@ export const POST = auth(async function POST(req) {
     return NextResponse.json({ message: "No session" }, { status: 401 });
   }
 
-  const requestBody = await req.json();
+  const requestBody: ResidentReqestApiRequest = await req.json();
 
-  console.info("Request body", requestBody);
+  const {
+    name,
+    areaCode,
+    phoneNumber,
+    googleAddressData,
+    appointmentDate,
+    timeSlot,
+  } = requestBody;
+
+  const { startTime, endTime } = assignRequestedTimeSlot(
+    appointmentDate,
+    timeSlot
+  );
+
+  // This route is assuming that external users are submitting the request on their own behalf
+  // Find the user in the database by e-mail, update their name to the one they entered on the form
+  try {
+    await prisma.user.update({
+      where: { email: req.auth.user?.email! },
+      data: {
+        name,
+        phoneNumber: `${areaCode}${phoneNumber}`,
+        address: {
+          create: {
+            streetNumber: (googleAddressData.address as Partial<Address>)
+              .streetNumber!,
+            city: (googleAddressData.address as Partial<Address>).city!,
+            zipCode: (googleAddressData.address as Partial<Address>).zipCode!,
+            latitude: (googleAddressData.address as Partial<Address>).latitude!,
+            longitude: (googleAddressData.address as Partial<Address>)
+              .longitude!,
+            streetName: (googleAddressData.address as Partial<Address>)
+              .streetName!,
+          },
+        },
+        requests: {
+          create: {
+            requestedTimeSlot: {
+              create: {
+                startTime: startTime,
+                endTime: endTime,
+              },
+            },
+          },
+        },
+      },
+      include: {
+        address: true,
+        requests: true,
+      },
+    });
+  } catch (error: any) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Failed to create resident request" },
+      { status: 500 }
+    );
+  }
+
   return NextResponse.json({ message: "Request received" });
 });
