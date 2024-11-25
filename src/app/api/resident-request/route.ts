cdb4131ac469fb7bb1fd5574e2af7ef3b43e43fe
oldmcdonnell <oldmcdonnell@gmail.com>
2024-11-25 14:54:17 -0500

    adding GET method to route.ts

--- src/app/api/resident-request/route.ts
+++ src/app/api/resident-request/route.ts
@@ -3,7 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { assignRequestedTimeSlot } from "@/lib/utils/time-slot/time-slot-assigners";
 import { residentRequestValidationSchema } from "@/lib/validation-schemas/validation-schemas";
 import { ResidentReqestApiRequest } from "@/types/resident-request-api-request";
-import { Address } from "@prisma/client";
+import { Address, RequestStatus  } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { ZodError } from "zod";
@@ -86,4 +86,52 @@ export async function POST(req: NextRequest) {
   }
 
   return NextResponse.json({ message: "Request received" });
+}
+
+
+//adding GET method
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: "No session" }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(req.url);
+  const status = searchParams.get("status");
+
+  // Validate the status parameter
+  if (!status) {
+    return NextResponse.json({ error: "Missing status parameter" }, { status: 400 });
+  }
+
+  const validStatuses: RequestStatus[] = [
+    RequestStatus.COMPLETED,
+    RequestStatus.PENDING,
+    RequestStatus.CANCELED,
+  ];
+ 
+ 
+  if (!validStatuses.includes(status as RequestStatus)) {
+    return NextResponse.json({ error: "Invalid status parameter" }, { status: 400 });
+  }
+ 
+  try {
+    const residentRequests = await prisma.residentRequest.findMany({
+      where: { status: status as RequestStatus }, 
+      include: {
+        user: true, 
+        requestedTimeSlot: true, 
+      },
+    });
+ 
+    return NextResponse.json(residentRequests);
+  } catch (error) {
+    console.error("Error fetching resident request:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch resident request" },
+      { status: 500 }
+    );  
+  }
 }
