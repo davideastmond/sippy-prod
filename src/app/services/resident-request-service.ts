5c751b55f7db274a60b416edb814c83ae29143d3
David Eastmond <davebuck1981@gmail.com>
2024-11-29 16:08:43 -0500

    user cancel request functionality and error display

--- src/app/services/resident-request-service.ts
+++ src/app/services/resident-request-service.ts
@@ -1,5 +1,6 @@
 import { UserResidentRequestsApiResponse } from "@/types/api-responses/user-resident-requests-api-response";
 import { ResidentReqestApiRequest } from "@/types/resident-request-api-request";
+import { RequestStatus } from "@prisma/client";
 
 export const ResidentRequestService = {
   create: async (data: ResidentReqestApiRequest) => {
@@ -24,6 +25,21 @@ export const ResidentRequestService = {
 
     if (!response.ok) {
       throw new Error("Failed to get resident requests");
+    }
+
+    return response.json();
+  },
+  cancelRequestById: async (requestId: string) => {
+    const response = await fetch(`/api/resident-request/${requestId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status: RequestStatus.CANCELED }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete resident request");
     }
 
     return response.json();
