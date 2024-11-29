2cb6a13991eacf7528ea5b497e8a010ed9811244
David Eastmond <davebuck1981@gmail.com>
2024-11-28 23:13:58 -0500

    add types and build out the user dashboard

--- src/components/dashboards/Client-dashboard.tsx
+++ src/components/dashboards/Client-dashboard.tsx
@@ -1,12 +1,20 @@
 "use client";
+import { UserResidentRequestsApiResponse } from "@/types/api-responses/user-resident-requests-api-response";
+import { RequestStatus } from "@prisma/client";
 import { ResidentRequestService } from "app/services/resident-request-service";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import FormattedTimeSlotDateTime from "../formatted-time-slot-date-time.tsx/FormattedTimeSlotDateTime";
+import Spinner from "../spinner/Spinner";
 export default function ClientDashboard() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
+  const [requestsForUser, setRequestsForUser] =
+    useState<UserResidentRequestsApiResponse>();
+
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     if (session?.user?.isAdmin) {
       router.replace("/dashboard/admin");
@@ -22,12 +30,16 @@ export default function ClientDashboard() {
 
   const fetchResidentRequestsForUser = async () => {
     try {
+      setIsLoading(true);
       const res =
         await ResidentRequestService.getResidentRequestsByAuthenticatedUser(
           session?.user?.id!
         );
+      setRequestsForUser(res);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   };
 
@@ -37,24 +49,74 @@ export default function ClientDashboard() {
     }
   }, [status]);
 
+  if (isLoading) {
+    return (
+      <div className="flex justify-center mt-[10%]">
+        <Spinner />
+      </div>
+    );
+  }
   return (
     <>
       <div>
         <h1>Client Dashboard</h1>
       </div>
       <h1>Welcome, {session?.user?.name} </h1>
-      <div className="mt-6">
-        <table>
-          <thead>
+      <div className="relative overflow-x-auto mt-6 p-2">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th>Address</th>
-              <th>Requested Appointment Time</th>
-              <th>Confirmed ETA</th>
-              <th>Status</th>
+              <th scope="col" className="px-6 py-3">
+                Address
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Req. Appt. Time
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Confirmed ETA
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Status
+              </th>
             </tr>
           </thead>
+          <tbody>
+            {requestsForUser?.requests.map((request) => (
+              <tr
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:cursor-pointer hover:bg-gray-50 dark:hover:bg-gray-700"
+                key={request?.id}
+                onClick={() => router.push(`/resident-request/${request?.id}`)}
+              >
+                <td className="px-6 py-4">
+                  {request.address?.streetNumber} {request.address?.streetName},{" "}
+                  {request.address?.city}
+                </td>
+                <td className="px-6 py-4">
+                  <FormattedTimeSlotDateTime
+                    start={request.requestedTimeSlot?.startTime}
+                    end={request.requestedTimeSlot?.endTime}
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <FormattedTimeSlotDateTime
+                    start={request.assignedTimeSlot?.startTime}
+                    end={request.assignedTimeSlot?.endTime}
+                  />
+                </td>
+                <td className={`${statusMapColor[request.status]} text-center`}>
+                  {request.status}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </>
   );
 }
+
+const statusMapColor = {
+  [RequestStatus.CANCELED]: "bg-simmpy-red",
+  [RequestStatus.COMPLETED]: "bg-simmpy-green",
+  [RequestStatus.PENDING]: "bg-simmpy-yellow",
+};
---
+++ src/types/api-responses/user-resident-requests-api-response.ts
@@ -0,0 +1,27 @@
+import { Prisma } from "@prisma/client";
+
+/* Reference: https://www.prisma.io/docs/orm/prisma-client/type-safety/operating-against-partial-structures-of-model-types
+  This is to try to get type safety when we retrieve data from the API that has a partial structure of a model's type.
+  For example, when we want to fetch all the requests for some user, the response has a partial structure of the User model type plus some select statements
+  that reference data from related tables (requested timeSlot, address etc)
+
+ This approach is supposed to be scalable and prevent us from having to write a lot of types for every possible combination of data we might want to fetch from the API
+ */
+
+const userWithResidentRequests = Prisma.validator<Prisma.UserDefaultArgs>()({
+  include: {
+    requests: {
+      select: {
+        id: true,
+        requestedTimeSlot: { select: { startTime: true, endTime: true } },
+        assignedTimeSlot: { select: { startTime: true, endTime: true } },
+        address: true,
+        status: true,
+      },
+    },
+  },
+});
+
+export type UserResidentRequestsApiResponse = Prisma.UserGetPayload<
+  typeof userWithResidentRequests
+>;
