b20b9c6017687b186030349c4190a65e2382b718
David Eastmond <davebuck1981@gmail.com>
2024-12-01 16:25:38 -0500

    fix props spellings causing complaints

--- src/components/search-requests-filter-panel/Search-requests-filter-panel.tsx
+++ src/components/search-requests-filter-panel/Search-requests-filter-panel.tsx
@@ -86,9 +86,9 @@ const SearchRequestsFilterPanel: React.FC<SearchRequestsProps> = ({
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="m1 1 4 4 4-4"
             />
           </svg>
