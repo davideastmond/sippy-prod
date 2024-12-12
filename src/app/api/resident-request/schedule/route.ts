75cb0abcfc5935bef9c57d14a5b04d24bd03f693
David Eastmond <davebuck1981@gmail.com>
2024-12-12 18:36:21 -0500

    route protection

--- src/app/api/resident-request/schedule/route.ts
+++ src/app/api/resident-request/schedule/route.ts
@@ -1,8 +1,14 @@
+import { authOptions } from "@/auth";
 import { getAvailableRequestedTimeSlotsByDate } from "@/lib/utils/time-slot/availability/get-available-requested-timeslot-by-date";
 import { TimeSlot } from "@/types/time-slot";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return NextResponse.json({ message: "No session" }, { status: 401 });
+  }
   const { searchParams } = new URL(req.url);
   // Expecting a date string in the format "YYYY-MM-DD"
   const dateQuery = searchParams.get("date");
