20599348c2db329ee218f22e225f3a8b8b2700da
David Eastmond <davebuck1981@gmail.com>
2024-12-16 10:33:31 -0500

    remove console logs. Delete unused route

--- src/app/api/request-schedule/route.ts
+++ src/app/api/request-schedule/route.ts
@@ -1,10 +1,9 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
 import { computeRouteMatrix } from "@/lib/utils/google-routes";
+import { getTimeSlotHours } from "@/lib/utils/time-slot/time-slot";
 import { TimeSlot } from "@/types/time-slot";
 import dayjs from "dayjs";
-// import { object } from "zod";
-import { getTimeSlotHours } from "@/lib/utils/time-slot/time-slot";
+import { NextResponse } from "next/server";
 
 interface Route {
   originIndex: number;
@@ -13,24 +12,53 @@ interface Route {
   condition: string;
 }
 
-// interface RequestBody {
-//   date: string; // Expected date input in "YYYY-MM-DD" format
-// }
-
-function determineTimeSlot(accumulatedTime: number, dateContext: Date):{startTime : Date, endTime: Date} {
-  console.log("determineTimeSlot", accumulatedTime, dateContext);
+function determineTimeSlot(
+  accumulatedTime: number,
+  dateContext: Date
+): { startTime: Date; endTime: Date } {
   const hour = Math.floor(accumulatedTime / 3600); // Convert seconds to hours
-  if (hour >= getTimeSlotHours(TimeSlot.Morning)[0] && hour < getTimeSlotHours(TimeSlot.Morning)[1]) 
-    return {startTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Morning)[0]).toDate(),
-      endTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Morning)[1]).toDate(),
+
+  if (
+    hour >= getTimeSlotHours(TimeSlot.Morning)[0] &&
+    hour < getTimeSlotHours(TimeSlot.Morning)[1]
+  )
+    return {
+      startTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Morning)[0])
+        .toDate(),
+      endTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Morning)[1])
+        .toDate(),
     };
-  if (hour >= getTimeSlotHours(TimeSlot.Daytime)[0] && hour < getTimeSlotHours(TimeSlot.Daytime)[1]) 
-    return {startTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Daytime)[0]).toDate(),
-      endTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Daytime)[1]).toDate(),
+  if (
+    hour >= getTimeSlotHours(TimeSlot.Daytime)[0] &&
+    hour < getTimeSlotHours(TimeSlot.Daytime)[1]
+  )
+    return {
+      startTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Daytime)[0])
+        .toDate(),
+      endTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Daytime)[1])
+        .toDate(),
     };
-  if (hour >= getTimeSlotHours(TimeSlot.Evening)[0] && hour < getTimeSlotHours(TimeSlot.Evening)[1]) 
-    return {startTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Evening)[0]).toDate(),
-      endTime: dayjs(dateContext).add(1, "day").set("hours", getTimeSlotHours(TimeSlot.Evening)[1]).toDate(),
+  if (
+    hour >= getTimeSlotHours(TimeSlot.Evening)[0] &&
+    hour < getTimeSlotHours(TimeSlot.Evening)[1]
+  )
+    return {
+      startTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Evening)[0])
+        .toDate(),
+      endTime: dayjs(dateContext)
+        .add(1, "day")
+        .set("hours", getTimeSlotHours(TimeSlot.Evening)[1])
+        .toDate(),
     };
 
   throw new Error("Unable to determine time slot for the given hour");
@@ -49,7 +77,10 @@ function optimizeRoute(routeMatrix: number[][]): number[] {
     let nearestNeighbor = -1;
     let shortestDistance = Infinity;
     for (let i = 0; i < numLocations; i++) {
-      if (!visited.has(i) && routeMatrix[currentLocation][i] < shortestDistance) {
+      if (
+        !visited.has(i) &&
+        routeMatrix[currentLocation][i] < shortestDistance
+      ) {
         shortestDistance = routeMatrix[currentLocation][i];
         nearestNeighbor = i;
       }
@@ -66,7 +97,6 @@ export async function POST(req: Request) {
   try {
     const requestBody = await req.json();
     const { date } = requestBody;
-    console.log("Backend Received request with date:", date);
 
     if (!date) {
       console.error("Missing 'date' in request body");
@@ -76,9 +106,7 @@ export async function POST(req: Request) {
       );
     }
     const requestStartTime = new Date(date);
-    const requestEndTime = dayjs(date).set("hour", 23).set("minute", 59).set("second", 59).toDate();
-    console.log("req date object with date:", requestStartTime);
-    console.log("req date object end time:", requestEndTime);
+
     // Fetch requests for the specific date and status PENDING
     const residentRequests = await prisma.residentRequest.findMany({
       where: {
@@ -96,9 +124,11 @@ export async function POST(req: Request) {
         address: true,
       },
     });
-    console.log("line 87");
+
     if (residentRequests.length === 0) {
-      return NextResponse.json({ message: "No requests found for the given date." });
+      return NextResponse.json({
+        message: "No requests found for the given date.",
+      });
     }
 
     const optimizedResults = [];
@@ -120,13 +150,15 @@ export async function POST(req: Request) {
       return map;
     }, {} as Record<string, Route>);
     // Create a route matrix
-    const routeMatrix: number[][] = Array.from({ length: locations.length }, () =>
-      Array(locations.length).fill(Infinity)
+    const routeMatrix: number[][] = Array.from(
+      { length: locations.length },
+      () => Array(locations.length).fill(Infinity)
     );
 
     routes.forEach((route) => {
       const durationInSeconds = parseInt(route.duration.replace("s", ""), 10);
-      routeMatrix[route.originIndex][route.destinationIndex] = durationInSeconds;
+      routeMatrix[route.originIndex][route.destinationIndex] =
+        durationInSeconds;
     });
 
     // Optimize route
@@ -143,12 +175,10 @@ export async function POST(req: Request) {
 
       const travelTime = routeMatrix[index][nextIndex];
       const timeSlot = determineTimeSlot(accumulatedTime, new Date(date));
-      console.log("determined timeSlot:", timeSlot);
 
       // Increment accumulated time by the stop duration and travel time
       accumulatedTime += stopDuration + travelTime;
-      console.log("line 135 cree req + id:", currentRequest.id );
-      
+
       // Update the assigned time slot in the database
       const updatedRequest = await prisma.residentRequest.update({
         where: { id: currentRequest.id },
@@ -157,11 +187,10 @@ export async function POST(req: Request) {
             create: {
               startTime: timeSlot.startTime,
               endTime: timeSlot.endTime,
-            }
+            },
           },
         },
       });
-      console.log("updatedRequest:", updatedRequest); 
       optimizedResults.push({
         ...updatedRequest,
         address: currentRequest.address,
@@ -170,10 +199,12 @@ export async function POST(req: Request) {
         route: routeMap[`${index}-${nextIndex}`],
       });
     }
-    console.log("optimizedResults:", JSON.stringify(optimizedResults, null, 2));
     return NextResponse.json(optimizedResults);
   } catch (error) {
     console.error("Error fetching or updating requests:", error);
-    return NextResponse.json({ error: "request schedule failed" }, { status: 500 });
+    return NextResponse.json(
+      { error: "request schedule failed" },
+      { status: 500 }
+    );
   }
 }
