361fb5f0ed2ff437fa690668c681479bc7e37357
David Eastmond <davebuck1981@gmail.com>
2024-12-16 10:36:08 -0500

    clean-up

--- src/app/services/collateDailyRequests/collateDailyRequests.ts
+++ src/app/services/collateDailyRequests/collateDailyRequests.ts
@@ -1,17 +1,9 @@
 import { ResidentRequestCollation } from "@/types/resident-request-collation";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
 
 export async function collateDailyRequests(
-  date: string,
+  date: string
 ): Promise<ResidentRequestCollation[]> {
   try {
-    console.log("Sending request to backend with date:", date); // Log before sending the request
-
     // Send POST request to backend with the selected date
     const response = await fetch(`/api/request-schedule`, {
       method: "POST",
@@ -22,15 +14,10 @@ export async function collateDailyRequests(
     });
 
     if (!response.ok) {
-      const errorText = await response.text(); // Read the error message from the response
-      console.error("Backend error response:", errorText); // Log the error response
       throw new Error(`Failed to fetch requests: ${response.statusText}`);
     }
 
     const requestAtDate: ResidentRequestCollation[] = await response.json();
-    console.log("Fetched all requests from API:", requestAtDate);
-
-    // Adjust UTC times to the client's local time zone
 
     return requestAtDate;
   } catch (error) {
