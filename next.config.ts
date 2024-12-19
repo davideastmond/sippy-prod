a1223af5917fe0476d762b2df4fd2a7f253e84ca
David Eastmond <davebuck1981@gmail.com>
2024-12-19 17:59:52 -0500

    redirection

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/resident-request",
+        destination: "/resident-request/new",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
