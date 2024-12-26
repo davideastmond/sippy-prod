cd5cebc22cb8aa4585e4fd9a6caedbeedf14cc97
David Eastmond <davebuck1981@gmail.com>
2024-12-26 11:27:58 -0500

    remove test route. Secure route

--- src/app/api/test/route.ts
+++
@@ -1,7 +0,0 @@
-
-import { NextResponse } from "next/server";
-
-//TODO This is just for test. Remove this file
-export async function GET() {
-  return NextResponse.json({ message: "Hello from the API!" });
-}
